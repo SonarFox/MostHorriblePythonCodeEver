aadd5eeaea36227ea9c6b6fbc8a6219737aaeaa8
Lee Fox <lee.fox@sonarsource.com>
2024-07-23 16:13:36 -0500

    Injecting Logging Injection

--- SessionCookiesFromUntrustedInput.py
+++ SessionCookiesFromUntrustedInput.py
@@ -1,13 +1,31 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
 from flask import Flask, request, make_response
 
+# Flask constructor takes the name of
+# current module (__name__) as argument.
 app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# '/' URL is bound with welcome() function.
+def welcome():
+    return "Welcome to the Flask Web Server!"
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
 
 @app.route('/login', methods=['POST'])
 def login():
+
     user_id = request.form.get('user_id')
     response = make_response("Logged in")
     response.set_cookie('session_id', user_id)  # Vulnerable: setting session cookie from untrusted input
     return response
 
-if __name__ == "__main__":
-    app.run(debug=True)
