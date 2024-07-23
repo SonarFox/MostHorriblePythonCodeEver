feba51090020e8745896bbdc52cd1941ef9c909a
Lee Fox <lee.fox@sonarsource.com>
2024-07-23 16:05:08 -0500

    Injecting Logging Injection

--- CommandInjection.py
+++ CommandInjection.py
@@ -1,8 +1,20 @@
 import os
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, request
 
-def vulnerable_function(user_input):
-    os.system(f'echo {user_input}')
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# '/' URL is bound with welcome() function.
+def welcome():
+    return "Welcome to the Flask Web Server!"
 
 if __name__ == "__main__":
-    user_input = input("Enter a string: ")
-    vulnerable_function(user_input)
+    data = request.args.get("data", "")
+    os.system(f'echo {data}')
