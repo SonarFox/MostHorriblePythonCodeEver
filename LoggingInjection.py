7d728bdcd646246b26e40fcf6966dc48ac71659a
Lee Fox <lee.fox@sonarsource.com>
2024-07-23 15:35:52 -0500

    Injecting Logging Injection

--- LoggingInjection.py
+++ LoggingInjection.py
@@ -1,8 +1,20 @@
 import logging
+from flask import Flask, request
 
 app = Flask(__name__)
 
+# Setup logger
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+
 @app.route('/example')
 def log():
-    data = request.args["data"]
-    app.logger.critical("%s", data) # Noncompliant
+    data = request.args.get("data", "")
+    app.logger.critical("%s", data)  # Noncompliant
+    return "Logged data"
+
+if __name__ == "__main__":
+    app.run(debug=True)
