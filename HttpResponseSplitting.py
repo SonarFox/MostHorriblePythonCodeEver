9e83904e308759d12bc2601541ba5b7cedc583b9
Lee Fox <lee.fox@sonarsource.com>
2024-07-24 12:04:38 -0500

    Injecting Logging Injection

--- HttpResponseSplitting.py
+++ HttpResponseSplitting.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/search')
 def search():
     query = request.args.get('q')
+    test = "test"
     return f"<h1>Search Results for: {query}</h1>"
 
 if __name__ == "__main__":
